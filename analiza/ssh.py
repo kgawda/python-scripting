9acf2692827cce176a88d3ae6aa13ec1c1d9073d
Konrad Gawda <k.gawda@alx.pl>
2022-10-07 16:13:38 +0200

    Pobieranie pliku

--- analiza/ssh.py
+++ analiza/ssh.py
@@ -3,12 +3,15 @@ import fabric
 with open('haslo.txt') as f:
     password = f.read().strip()
 
-result = fabric.Connection(
+connection = fabric.Connection(
     '54.75.110.104',
     user='konrad',
     port=7501,
     connect_kwargs={"password": password}
-).run('df -h')
+)
 
+connection.run('ls -al logs')
 # result.stdout
 # result.ok
+
+connection.get('/config/logs/openssh/current', local='openssh.log')
